601f19d65aa6f2c30e0edb2757183fe81b69c8bd
Deepak Sunanda Prabhakar <deepak.prabhakar@trendyol.com>
2024-11-04 11:24:30 +0100

    add comments

--- if.go
+++ if.go
@@ -1,18 +1,27 @@
 package Condigo
 
+// Conditional represents the initial condition that is evaluated.
+// If the condition is true, Then logic will execute.
 type Conditional struct {
 	condition bool
 }
 
+// ElseIf represents the state after a Then or ElseIf condition.
+// It keeps track of whether a result has been returned and holds the last result.
 type ElseIf struct {
-	returned   bool
-	lastResult interface{}
+	returned   bool        // Tracks if a condition has already been met
+	lastResult interface{} // Stores the result to return if this condition is met
 }
 
+// When initializes a Conditional with a given condition.
+// Returns a pointer to Conditional for further chaining.
 func When(condition bool) *Conditional {
 	return &Conditional{condition: condition}
 }
 
+// Then checks if the initial condition was true.
+// If so, it stores the result and marks the condition as met by setting returned to true.
+// Otherwise, it continues to the next condition (e.g., ElseIf).
 func (c *Conditional) Then(result interface{}) *ElseIf {
 	if c.condition {
 		return &ElseIf{returned: true, lastResult: result}
@@ -20,6 +29,9 @@ func (c *Conditional) Then(result interface{}) *ElseIf {
 	return &ElseIf{returned: false, lastResult: nil}
 }
 
+// ElseIf checks an additional condition if previous conditions (When or earlier ElseIf)
+// have not been met. If this condition is true, it stores the result and marks
+// the condition as met. If not, it continues to the next condition.
 func (ei *ElseIf) ElseIf(condition bool, result interface{}) *ElseIf {
 	if !ei.returned && condition {
 		return &ElseIf{returned: true, lastResult: result}
@@ -27,6 +39,9 @@ func (ei *ElseIf) ElseIf(condition bool, result interface{}) *ElseIf {
 	return &ElseIf{returned: ei.returned, lastResult: ei.lastResult}
 }
 
+// Else provides a default result if none of the previous conditions (When, Then, or ElseIf) were met.
+// If no conditions were met, it returns the defaultResult.
+// If any condition was met, it returns the result of the first matched condition.
 func (ei *ElseIf) Else(defaultResult interface{}) interface{} {
 	if !ei.returned {
 		return defaultResult
